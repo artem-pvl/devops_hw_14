922317a7581c282c1424621a08ef57b8ca3fc8b9
Artem Pavlichenko <artem.pvl@gmail.com>
2021-10-27 07:11:30 +0300

    all

--- config.tf
+++ config.tf
@@ -40,16 +40,24 @@ resource "aws_key_pair" "aws_key_t" {
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
   description = "Allow all"
-  vpc_id      = aws_vpc.main.id
 
   ingress = [
     {
-      description      = "TLS from VPC"
-      from_port        = 443
-      to_port          = 443
+      from_port        = 0
+      to_port          = 0
       protocol         = "tcp"
-      cidr_blocks      = [aws_vpc.main.cidr_block]
-      ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
     }
   ]
 
